af8b0bdb06eaed7dacec717c2ee83469e64b0d1f
stephane brossier <sbrossier@groupon.com>
2014-10-03 15:16:16 -0700

    Fix wrong package name introduced in previous commit

--- jdbi/src/main/java/org/killbill/commons/jdbi/mapper/LowerToCamelBeanMapper.java
+++ jdbi/src/main/java/org/killbill/commons/jdbi/mapper/LowerToCamelBeanMapper.java
@@ -14,7 +14,7 @@
  * under the License.
  */
 
-package org.killbill.billing.util.dao;
+package org.killbill.commons.jdbi.mapper;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
