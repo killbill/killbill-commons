killbill/killbill-commons

commit 5c5ef76e72571080a3b5ce8acedefaee4b049b39

Remove limitations in the number of events that can be posted in one transaction

queue/src/main/java/org/killbill/queue/DBBackedQueue.java
@@ -81,9 +81,6 @@ public class DBBackedQueue<T extends org.killbill.queue.dao.EventEntryModelDao> 
     //
     private final static long INFLIGHT_POLLING_TIMEOUT_MSEC = 50;
 
-    // Maximum number of events that can be set from within one transaction (we could make it a config param if required)
-    private static final int MAX_BUS_ENTRIES_PER_TRANSACTIONS = 3;
-
 
     //
     // When running with inflightQ, add a polling every 5 minutes to detect if there are
@@ -703,7 +700,7 @@ public class DBBackedQueue<T extends org.killbill.queue.dao.EventEntryModelDao> 
         public void addRowId(final Long rowId) {
             RowRef entry = rowRefThreadLocal.get();
             if (entry == null) {
-                entry = new RowRef(queueId, rowId);
+                entry = new RowRef(queueId);
                 rowRefThreadLocal.set(entry);
             } else {
                 entry.addRowId(rowId);
@@ -725,45 +722,19 @@ public class DBBackedQueue<T extends org.killbill.queue.dao.EventEntryModelDao> 
         private final class RowRef {
 
             private final int queueId;
-            private final long[] rowIds;
+            private final List<Long> rowIds;
 
-            private int offset;
-
-            public RowRef(int queueId, long initialRowId) {
+            public RowRef(int queueId) {
                 this.queueId = queueId;
-                this.rowIds = new long[MAX_BUS_ENTRIES_PER_TRANSACTIONS];
-                this.offset = 0;
-                this.rowIds[offset] = initialRowId;
+                this.rowIds = new ArrayList<Long>();
             }
 
             public void addRowId(long rowId) {
-                if (offset == MAX_BUS_ENTRIES_PER_TRANSACTIONS - 1) {
-                    log.error("DBBackedQ- Thread " + Thread.currentThread().getId() + "DBBackedQueue InflightQ thread local variable for queue " + queueId + " has too many entries, and was probably not reset correctly! offset = " + offset);
-                    return;
-                }
-                rowIds[++offset] = rowId;
+                rowIds.add(rowId);
             }
 
             public Iterator<Long> iterator() {
-                return new Iterator<Long>() {
-
-                    private int iteratorOffset = 0;
-
-                    @Override
-                    public boolean hasNext() {
-                        return (iteratorOffset <= offset);
-                    }
-
-                    @Override
-                    public Long next() {
-                        return rowIds[iteratorOffset++];
-                    }
-
-                    @Override
-                    public void remove() {
-                        throw new IllegalStateException();
-                    }
-                };
+                return rowIds.iterator();
             }
         }
     }

commit 8cd4329ea0017681c80f63c14a71968ba32d1f55

Fix regression introduced by 5c5ef76e72571080a3b5ce8acedefaee4b049b39 Fix DefaultPersistentBus to correctlycreate the DefaultPersistentBus config from properties
Note that new CTOR in DefaultPersistentBus that takes a Connection will ONLY work with org.killbill.persistent.bus.main.useInflightQ=false

queue/src/main/java/org/killbill/bus/DefaultPersistentBus.java
@@ -31,6 +31,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.sql.DataSource;
 
+import com.google.common.collect.ImmutableMap;
 import org.killbill.Hostname;
 import org.killbill.bus.api.BusEvent;
 import org.killbill.bus.api.PersistentBus;
@@ -93,7 +94,8 @@ public class DefaultPersistentBus extends DefaultQueueLifecycle implements Persi
     }
 
     public DefaultPersistentBus(final DataSource dataSource, final Properties properties) {
-        this(InTransaction.buildDDBI(dataSource), new DefaultClock(), new ConfigurationObjectFactory(new SimplePropertyConfigSource(properties)).build(PersistentBusConfig.class), new MetricRegistry(), new DatabaseTransactionNotificationApi());
+        this(InTransaction.buildDDBI(dataSource), new DefaultClock(), new ConfigurationObjectFactory(properties).buildWithReplacements(PersistentBusConfig.class,
+                ImmutableMap.<String, String>of("instanceName", "main")), new MetricRegistry(), new DatabaseTransactionNotificationApi());
     }
 
     @Override

queue/src/main/java/org/killbill/queue/DBBackedQueue.java
@@ -702,9 +702,8 @@ public class DBBackedQueue<T extends org.killbill.queue.dao.EventEntryModelDao> 
             if (entry == null) {
                 entry = new RowRef(queueId);
                 rowRefThreadLocal.set(entry);
-            } else {
-                entry.addRowId(rowId);
             }
+            entry.addRowId(rowId);
         }
 
         public void reset() {

queue/src/test/java/org/killbill/bus/TestPersistentBusDemo.java
@@ -74,7 +74,7 @@ public class TestPersistentBusDemo {
         properties.setProperty("org.killbill.persistent.bus.main.sleep", "3000");
         properties.setProperty("org.killbill.persistent.bus.main.off", "false");
         properties.setProperty("org.killbill.persistent.bus.main.nbThreads", "1");
-        properties.setProperty("org.killbill.persistent.bus.main.useInflightQ", "true");
+        properties.setProperty("org.killbill.persistent.bus.main.useInflightQ", "false");
         properties.setProperty("org.killbill.persistent.bus.main.queue.capacity", "3000");
         properties.setProperty("org.killbill.persistent.bus.main.tableName", "bus_events");
         properties.setProperty("org.killbill.persistent.bus.main.historyTableName", "bus_events_history");