b1303ca69a3f723ee1e44b4025f00d785e0d6957
kares <self@kares.org>
2015-02-25 14:56:08 +0100

    do not let travis-ci error on JDK6 simply due an @Override annotation on an interface

--- jdbi/src/test/java/org/killbill/commons/jdbi/guice/TestDataSourceProvider.java
+++ jdbi/src/test/java/org/killbill/commons/jdbi/guice/TestDataSourceProvider.java
@@ -137,37 +137,30 @@ public class TestDataSourceProvider {
             }
         }
 
-        @Override
         public Connection connect(String url, Properties info) throws SQLException {
             return null;
         }
 
-        @Override
         public boolean acceptsURL(String url) throws SQLException {
             return url.contains("oracle");
         }
 
-        @Override
         public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
             return new DriverPropertyInfo[0];
         }
 
-        @Override
         public int getMajorVersion() {
             return 0;
         }
 
-        @Override
         public int getMinorVersion() {
             return 0;
         }
 
-        @Override
         public boolean jdbcCompliant() {
             return false;
         }
 
-        @Override
         public Logger getParentLogger() throws SQLFeatureNotSupportedException {
             return null;
         }
