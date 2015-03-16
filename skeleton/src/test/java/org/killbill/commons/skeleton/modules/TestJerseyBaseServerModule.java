ca607586cf6118bad1b8e32e744abee2c93799a6
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-03-16 06:23:31 -0700

    skeleton: upgrade to Jetty 9
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- skeleton/src/test/java/org/killbill/commons/skeleton/modules/TestJerseyBaseServerModule.java
+++ skeleton/src/test/java/org/killbill/commons/skeleton/modules/TestJerseyBaseServerModule.java
@@ -1,7 +1,9 @@
 /*
  * Copyright 2010-2014 Ning, Inc.
+ * Copyright 2014-2015 Groupon, Inc
+ * Copyright 2014-2015 The Billing Project, LLC
  *
- * Ning licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
@@ -19,6 +21,7 @@ package org.killbill.commons.skeleton.modules;
 import java.util.Map;
 import java.util.concurrent.Future;
 
+import org.eclipse.jetty.server.NetworkConnector;
 import org.eclipse.jetty.server.Server;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -35,7 +38,7 @@ public class TestJerseyBaseServerModule extends AbstractBaseServerModuleTest {
         final Server server = startServer(builder.build(), new HelloModule());
 
         final AsyncHttpClient client = new AsyncHttpClient();
-        final Future<Response> responseFuture = client.prepareGet("http://127.0.0.1:" + server.getConnectors()[0].getPort() + "/hello/alhuile/").execute();
+        final Future<Response> responseFuture = client.prepareGet("http://127.0.0.1:" + ((NetworkConnector) server.getConnectors()[0]).getPort() + "/hello/alhuile/").execute();
         final String body = responseFuture.get().getResponseBody();
         Assert.assertEquals(body, "Hello alhuile");
 
