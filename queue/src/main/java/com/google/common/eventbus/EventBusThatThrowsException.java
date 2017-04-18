2657d576f5f833d11633ef4fe5f3c31d241e9788
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-04-18 09:47:27 -0700

    queue: re-implement EventBusThatThrowsException for Guava 20.0
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- queue/src/main/java/com/google/common/eventbus/EventBusThatThrowsException.java
+++ queue/src/main/java/com/google/common/eventbus/EventBusThatThrowsException.java
@@ -1,7 +1,9 @@
 /*
  * Copyright 2010-2014 Ning, Inc.
+ * Copyright 2014-2017 Groupon, Inc
+ * Copyright 2014-2017 The Billing Project, LLC
  *
- * Ning licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
@@ -16,130 +18,117 @@
 
 package com.google.common.eventbus;
 
-import com.google.common.collect.SetMultimap;
-import org.killbill.bus.api.BusEvent;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
-import java.util.Queue;
-import java.util.Set;
-import java.util.concurrent.locks.ReadWriteLock;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.google.common.util.concurrent.MoreExecutors;
 
 /**
- * Bus exceptions in the guava framework are swallowed  and that sucks.
+ * Bus exceptions in the guava framework are swallowed and that sucks.
  * <p/>
  * I sumitted a ticket https://code.google.com/p/guava-libraries/issues/detail?id=981 which was then merged into
  * https://code.google.com/p/guava-libraries/issues/detail?id=780
  * <p/>
  * They closed the bug, but i am still not seeing any way to get those exceptions back, so since we REALLY
- * need it, we have to hack the code:
- * <p/>
- * There is a new postWithException method that will throw an EventBusException if there is any exceptions during
- * the dispatch phase, which means that some handlers might have been called successfully and others not, which is
- * fine with our 'at least once' delivery semantics.
- *
- * NOTE that starting in guava-16.0 there is a new mechanism where one can register a `SubscriberExceptionHandler`, but
- * that does not quite fit our needs because we are processing events in a loop and the context of that loop needs to be taken
- * into consideration to know how to handle the exception, so we are keeping the hack below.
- *
+ * need it, we have to hack the code.
  */
 public class EventBusThatThrowsException extends EventBus {
 
-    public EventBusThatThrowsException(String identifier) {
-        super(identifier);
+    private final SubscriberRegistry subscribers;
+    private final Dispatcher dispatcher;
+    private final SubscriberExceptionsTrackerHandler exceptionHandler;
+
+    public EventBusThatThrowsException(final String identifier) {
+        super(identifier,
+              // See postWithException below
+              MoreExecutors.directExecutor(),
+              Dispatcher.immediate(),
+              new SubscriberExceptionsTrackerHandler());
+        this.subscribers = getSubscribers();
+        this.dispatcher = getDispatcher();
+        this.exceptionHandler = getExceptionHandler();
     }
 
-    public void postWithException(Object event) throws EventBusException {
-        Set dispatchTypes = this.flattenHierarchy(event.getClass());
-        boolean dispatched = false;
-        Iterator i$ = dispatchTypes.iterator();
-
-        while(i$.hasNext()) {
-            Class eventType = (Class)i$.next();
-            getSubscribersByTypeLock().readLock().lock();
-
+    public void postWithException(final Object event) throws EventBusException {
+        final Iterator<Subscriber> eventSubscribers = subscribers.getSubscribers(event);
+        if (eventSubscribers.hasNext()) {
+            RuntimeException guavaException = null;
+            final Exception subscriberException;
             try {
-                Set wrappers = getSubscribersByType().get(eventType);
-                if(!wrappers.isEmpty()) {
-                    dispatched = true;
-                    Iterator i$1 = wrappers.iterator();
-
-                    while(i$1.hasNext()) {
-                        EventSubscriber wrapper = (EventSubscriber)i$1.next();
-                        this.enqueueEvent(event, wrapper);
-                    }
-                }
+                dispatcher.dispatch(event, eventSubscribers);
+            } catch (final RuntimeException e) {
+                guavaException = e;
             } finally {
-                getSubscribersByTypeLock().readLock().unlock();
-            }
-        }
-
-        if(!dispatched && !(event instanceof DeadEvent)) {
-            this.post(new DeadEvent(this, event));
-        }
-
-        dispatchQueuedEventsWithException();
-    }
-
-    void dispatchQueuedEventsWithException() throws EventBusException {
-        if(!((Boolean)getIsDispatching().get()).booleanValue()) {
-            getIsDispatching().set(Boolean.valueOf(true));
-
-            try {
-                Queue events = (Queue)getEventsToDispatch().get();
-
-                EventBus.EventWithSubscriber eventWithSubscriber;
-                while((eventWithSubscriber = (EventBus.EventWithSubscriber)events.poll()) != null) {
-                    dispatchWithException(eventWithSubscriber.event, eventWithSubscriber.subscriber);
-                }
-            } finally {
-                getIsDispatching().remove();
-                getIsDispatching().remove();
+                // This works because we are both using the immediate dispatcher and the direct executor
+                // Note: we always want to dequeue here to avoid any memory leaks
+                subscriberException = exceptionHandler.caught(event);
             }
 
+            if (guavaException != null) {
+                throw guavaException;
+            }
+            if (subscriberException != null) {
+                throw new EventBusException(subscriberException);
+            }
+        } else if (!(event instanceof DeadEvent)) {
+            // the event had no subscribers and was not itself a DeadEvent
+            post(new DeadEvent(this, event));
         }
     }
-
-    void dispatchWithException(Object event, EventSubscriber wrapper) throws EventBusException {
-        try {
-            wrapper.handleEvent(event);
-        } catch (InvocationTargetException e) {
-            throw new EventBusException(e);
-        }
-    }
-
 
     //
     // Even more ugliness to access private fields from the BusEvent class. Obviously this is very fragile;
     // if they decide to just rename their field name field, all hell breaks loose. So updating guava will
     //  require some attention-- but none of our tests would work so we should also see it pretty quick!
     //
-    private ReadWriteLock getSubscribersByTypeLock() throws EventBusException {
-        return getDeclaredField("subscribersByTypeLock");
+
+    private SubscriberRegistry getSubscribers() {
+        return getDeclaredField("subscribers");
     }
 
-    private SetMultimap<Class<?>, EventSubscriber> getSubscribersByType() throws EventBusException {
-        return getDeclaredField("subscribersByType");
+    private Dispatcher getDispatcher() {
+        return getDeclaredField("dispatcher");
     }
 
-    private ThreadLocal<Boolean> getIsDispatching() throws EventBusException {
-        return getDeclaredField("isDispatching");
+    private SubscriberExceptionsTrackerHandler getExceptionHandler() {
+        return getDeclaredField("exceptionHandler");
     }
 
-    private ThreadLocal<Queue<EventBus.EventWithSubscriber>> getEventsToDispatch() throws EventBusException {
-        return getDeclaredField("eventsToDispatch");
-    }
-
-    private <T> T getDeclaredField(final String fieldName) throws EventBusException {
+    private <T> T getDeclaredField(final String fieldName) {
         try {
             final Field f = EventBus.class.getDeclaredField(fieldName);
             f.setAccessible(true);
             return (T) f.get(this);
-        } catch (NoSuchFieldException e) {
-            throw new EventBusException("Failed to retrieve private field from BusEvent class " + fieldName, e);
-        } catch (IllegalAccessException e) {
-            throw new EventBusException("Failed to retrieve private field from BusEvent class " + fieldName, e);
+        } catch (final NoSuchFieldException e) {
+            throw new RuntimeException("Failed to retrieve private field from BusEvent class " + fieldName, e);
+        } catch (final IllegalAccessException e) {
+            throw new RuntimeException("Failed to retrieve private field from BusEvent class " + fieldName, e);
+        }
+    }
+
+    private static final class SubscriberExceptionsTrackerHandler implements SubscriberExceptionHandler {
+
+        private final Map<Object, Exception> eventsWithException = new ConcurrentHashMap<Object, Exception>();
+
+        private final SubscriberExceptionHandler loggerHandler = LoggingHandler.INSTANCE;
+
+        @Override
+        public void handleException(final Throwable exception, final SubscriberExceptionContext context) {
+            // By convention, re-throw the very first exception
+            final Object v = eventsWithException.get(context.getEvent());
+            if (v == null) {
+                // Wrapping for legacy reasons
+                eventsWithException.put(context.getEvent(), new InvocationTargetException(exception));
+            }
+
+            loggerHandler.handleException(exception, context);
+        }
+
+        public Exception caught(final Object event) {
+            return eventsWithException.remove(event);
         }
     }
 }
