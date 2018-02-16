killbill/killbill-commons

commit db8e582d9bbf0cb84894c5276ac910dbf79f864a

concurrent, locker, jdbi: Add new profiling type to measure time spent waiting for global lock and SQL connections

locker/src/main/java/org/killbill/commons/locker/GlobalLockBase.java
@@ -17,11 +17,14 @@
 
 package org.killbill.commons.locker;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import org.killbill.commons.profiling.Profiling;
+import org.killbill.commons.profiling.Profiling.WithProfilingCallback;
+import org.killbill.commons.profiling.ProfilingFeature.ProfilingFeatureType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GlobalLockBase implements GlobalLock {
 
@@ -33,6 +36,7 @@ public class GlobalLockBase implements GlobalLock {
     private final Connection connection;
     private final String lockName;
     private final ResetReentrantLockCallback resetCallback;
+    private final Profiling<Void, SQLException> prof;
 
 
     public GlobalLockBase(final Connection connection, final String lockName, final GlobalLockDao lockDao, final ResetReentrantLockCallback resetCallback) {
@@ -40,17 +44,25 @@ public class GlobalLockBase implements GlobalLock {
         this.connection = connection;
         this.lockName = lockName;
         this.resetCallback = resetCallback;
+        this.prof = new Profiling<Void, SQLException>();
     }
 
     @Override
     public void release() {
-        if (resetCallback != null && !resetCallback.reset(lockName)) {
-            // We are not the last one using that lock, bail early (AND don't close the connection)...
-            return;
-        }
-
         try {
-            lockDao.releaseLock(connection, lockName);
+            prof.executeWithProfiling(ProfilingFeatureType.GLOCK, "release", new WithProfilingCallback<Void, SQLException>() {
+                @Override
+                public Void execute() throws SQLException {
+
+                    if (resetCallback != null && !resetCallback.reset(lockName)) {
+                        // We are not the last one using that lock, bail early (AND don't close the connection)...
+                        return null;
+                    }
+                    lockDao.releaseLock(connection, lockName);
+
+                    return null;
+                }
+            });
         } catch (final SQLException e) {
             logger.warn("Unable to release lock for " + lockName, e);
         } finally {

concurrent/src/main/java/org/killbill/commons/profiling/ProfilingFeature.java
@@ -29,13 +29,17 @@ public class ProfilingFeature {
     private static final int API_MASK = 0x2;
     private static final int DAO_MASK = 0x4;
     private static final int DAO_DETAILS_MASK = 0x8;
-    private static final int PLUGIN_MASK = 0x10;
+    private static final int GLOCK_MASK = 0x10;
+    private static final int PLUGIN_MASK = 0x20;
+    private static final int DAO_CONNECTION_MASK = 0x40;
 
     public enum ProfilingFeatureType {
         JAXRS(JAXRS_MASK),
         API(API_MASK),
         DAO(DAO_MASK),
         DAO_DETAILS(DAO_MASK, DAO_DETAILS_MASK),
+        DAO_CONNECTION(DAO_CONNECTION_MASK),
+        GLOCK(GLOCK_MASK),
         PLUGIN(PLUGIN_MASK);
 
         private final int mask;
@@ -102,4 +106,13 @@ public class ProfilingFeature {
     public boolean isProfilingPlugin() {
         return isDefined(ProfilingFeatureType.PLUGIN);
     }
+
+    public boolean isProfilingGlock() {
+        return isDefined(ProfilingFeatureType.GLOCK);
+    }
+
+    public boolean isProfilingDaoConnection() {
+        return isDefined(ProfilingFeatureType.DAO_CONNECTION);
+    }
+
 }

jdbi/pom.xml
@@ -148,6 +148,10 @@
         </dependency>
         <dependency>
             <groupId>org.kill-bill.commons</groupId>
+            <artifactId>killbill-concurrent</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.kill-bill.commons</groupId>
             <artifactId>killbill-embeddeddb-common</artifactId>
         </dependency>
         <dependency>

jdbi/src/main/java/org/skife/jdbi/v2/DBI.java
@@ -15,6 +15,9 @@
  */
 package org.skife.jdbi.v2;
 
+import org.killbill.commons.profiling.Profiling;
+import org.killbill.commons.profiling.Profiling.WithProfilingCallback;
+import org.killbill.commons.profiling.ProfilingFeature.ProfilingFeatureType;
 import org.skife.jdbi.v2.exceptions.CallbackFailedException;
 import org.skife.jdbi.v2.exceptions.UnableToObtainConnectionException;
 import org.skife.jdbi.v2.logging.NoOpLog;
@@ -61,6 +64,9 @@ public class DBI implements IDBI
     private AtomicReference<SQLLog> log = new AtomicReference<SQLLog>(new NoOpLog());
     private AtomicReference<TimingCollector> timingCollector = new AtomicReference<TimingCollector>(TimingCollector.NOP_TIMING_COLLECTOR);
 
+
+    private final Profiling<Connection, SQLException> prof;
+
     /**
      * Constructor for use with a DataSource which will provide
      *
@@ -84,6 +90,7 @@ public class DBI implements IDBI
     {
         assert connectionFactory != null;
         this.connectionFactory = connectionFactory;
+        this.prof = new Profiling<Connection, SQLException>();
     }
 
     /**
@@ -207,7 +214,14 @@ public class DBI implements IDBI
     {
         try {
             final long start = System.nanoTime();
-            Connection conn = connectionFactory.openConnection();
+
+            Connection conn = prof.executeWithProfiling(ProfilingFeatureType.DAO_CONNECTION, "get", new WithProfilingCallback<Connection, SQLException>() {
+                @Override
+                public Connection execute() throws SQLException {
+                    return connectionFactory.openConnection();
+                }
+            });
+
             final long stop = System.nanoTime();
             StatementBuilder cache = statementBuilderFactory.get().createStatementBuilder(conn);
             Handle h = new BasicHandle(transactionhandler.get(),

locker/src/main/java/org/killbill/commons/locker/GlobalLockerBase.java
@@ -24,6 +24,9 @@ import java.util.concurrent.TimeUnit;
 import javax.sql.DataSource;
 
 import org.killbill.commons.locker.ReentrantLock.TryAcquireLockState;
+import org.killbill.commons.profiling.Profiling;
+import org.killbill.commons.profiling.Profiling.WithProfilingCallback;
+import org.killbill.commons.profiling.ProfilingFeature.ProfilingFeatureType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +42,7 @@ public abstract class GlobalLockerBase implements GlobalLocker {
     protected final ReentrantLock lockTable;
 
     private final DataSource dataSource;
+    private final Profiling<GlobalLock, LockFailedException> prof;
 
     public GlobalLockerBase(final DataSource dataSource, final GlobalLockDao globalLockDao, final long timeout, final TimeUnit timeUnit) {
         this.dataSource = dataSource;
@@ -46,24 +50,31 @@ public abstract class GlobalLockerBase implements GlobalLocker {
         this.timeUnit = timeUnit;
         this.globalLockDao = globalLockDao;
         this.lockTable = new ReentrantLock();
+        this.prof = new Profiling<GlobalLock, LockFailedException>();
     }
 
     @Override
     public GlobalLock lockWithNumberOfTries(final String service, final String lockKey, final int retry) throws LockFailedException {
-        final String lockName = getLockName(service, lockKey);
-        int tries_left = retry;
-        while (tries_left-- > 0) {
-            final GlobalLock lock = lock(lockName);
-            if (lock != null) {
-                return lock;
-            }
-            if (tries_left > 0) {
-                sleep();
-            }
-        }
 
-        logger.warn(String.format("Failed to acquire lock %s for service %s after %s retries", lockKey, service, retry));
-        throw new LockFailedException();
+         return prof.executeWithProfiling(ProfilingFeatureType.GLOCK, "lock", new WithProfilingCallback<GlobalLock, LockFailedException>() {
+            @Override
+            public GlobalLock execute() throws LockFailedException {
+                final String lockName = getLockName(service, lockKey);
+                int tries_left = retry;
+                while (tries_left-- > 0) {
+                    final GlobalLock lock = lock(lockName);
+                    if (lock != null) {
+                        return lock;
+                    }
+                    if (tries_left > 0) {
+                        sleep();
+                    }
+                }
+
+                logger.warn(String.format("Failed to acquire lock %s for service %s after %s retries", lockKey, service, retry));
+                throw new LockFailedException();
+            }
+        });
     }
 
     @Override

commit 966a01dd1fa64f4ec42f107248986c19155d0e78

locker: Fix issue introduced in db8e582d9bbf0cb where we close connection too early when unlocking

locker/src/main/java/org/killbill/commons/locker/GlobalLockBase.java
@@ -36,7 +36,7 @@ public class GlobalLockBase implements GlobalLock {
     private final Connection connection;
     private final String lockName;
     private final ResetReentrantLockCallback resetCallback;
-    private final Profiling<Void, SQLException> prof;
+    private final Profiling<Void, RuntimeException> prof;
 
 
     public GlobalLockBase(final Connection connection, final String lockName, final GlobalLockDao lockDao, final ResetReentrantLockCallback resetCallback) {
@@ -44,33 +44,32 @@ public class GlobalLockBase implements GlobalLock {
         this.connection = connection;
         this.lockName = lockName;
         this.resetCallback = resetCallback;
-        this.prof = new Profiling<Void, SQLException>();
+        this.prof = new Profiling<Void, RuntimeException>();
     }
 
     @Override
     public void release() {
-        try {
-            prof.executeWithProfiling(ProfilingFeatureType.GLOCK, "release", new WithProfilingCallback<Void, SQLException>() {
-                @Override
-                public Void execute() throws SQLException {
+        prof.executeWithProfiling(ProfilingFeatureType.GLOCK, "release", new WithProfilingCallback<Void, RuntimeException>() {
+            @Override
+            public Void execute() throws RuntimeException {
 
-                    if (resetCallback != null && !resetCallback.reset(lockName)) {
-                        // We are not the last one using that lock, bail early (AND don't close the connection)...
-                        return null;
-                    }
-                    lockDao.releaseLock(connection, lockName);
-
+                if (resetCallback != null && !resetCallback.reset(lockName)) {
+                    // We are not the last one using that lock, bail early (AND don't close the connection)...
                     return null;
                 }
-            });
-        } catch (final SQLException e) {
-            logger.warn("Unable to release lock for " + lockName, e);
-        } finally {
-            try {
-                connection.close();
-            } catch (final SQLException e) {
-                logger.warn("Unable to close connection", e);
+                try {
+                    lockDao.releaseLock(connection, lockName);
+                } catch (final SQLException e) {
+                    logger.warn("Unable to release lock for " + lockName, e);
+                } finally {
+                    try {
+                        connection.close();
+                    } catch (final SQLException e) {
+                        logger.warn("Unable to close connection", e);
+                    }
+                }
+                return null;
             }
-        }
+        });
     }
 }