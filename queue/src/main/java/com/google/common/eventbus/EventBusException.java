ace3bef982fadd8b0348df284904b979ba2674f1
Pierre-Alexandre Meyer <pierre@mouraf.org>
2020-07-10 05:26:22 +0100

    Update copyright headers
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- queue/src/main/java/com/google/common/eventbus/EventBusException.java
+++ queue/src/main/java/com/google/common/eventbus/EventBusException.java
@@ -1,11 +1,14 @@
 /*
  * Copyright 2010-2014 Ning, Inc.
+ * Copyright 2014-2020 Groupon, Inc
+ * Copyright 2020-2020 Equinix, Inc
+ * Copyright 2014-2020 The Billing Project, LLC
  *
- * Ning licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
