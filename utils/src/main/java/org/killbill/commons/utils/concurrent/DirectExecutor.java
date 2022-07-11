aabab544c9991e6510c96c07c00c327bd1242a9d
xsalefter <xsalefter@gmail.com>
2022-07-11 10:46:37 +0700

    add DirectExecutor, placed in utils modules to avoid circular dependency

---
+++ utils/src/main/java/org/killbill/commons/utils/concurrent/DirectExecutor.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020-2022 Equinix, Inc
+ * Copyright 2014-2022 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.killbill.commons.utils.concurrent;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Direct replacement of Guava {@code DirectExecutor}. Placed in {@code utils} instead of {@code concurrent} module
+ * to avoid circular dependency.
+ *
+ * An {@link Executor} that runs each task in the thread that invokes {@link Executor#execute}.
+ */
+public enum DirectExecutor implements Executor {
+
+    INSTANCE;
+
+    @Override
+    public void execute(final Runnable command) {
+        command.run();
+    }
+}
