a717543a4947860b7681b2e89a07e16d5da7bbf7
xsalefter <xsalefter@gmail.com>
2022-07-21 22:19:38 +0700

    add toCamelCase() and toSnakeCase() to Strings

--- utils/src/test/java/org/killbill/commons/utils/TestStrings.java
+++ utils/src/test/java/org/killbill/commons/utils/TestStrings.java
@@ -41,4 +41,44 @@ public class TestStrings {
     public void testContainsUpperCase(final String sample, final boolean valid) {
         Assert.assertEquals(Strings.containsUpperCase(sample), valid);
     }
+
+    @DataProvider(name = "testToCamelCase")
+    public Object[][] testToCamelCaseData() {
+        return new Object[][] {
+                {"this_is_lower_case", "thisIsLowerCase"},
+                {"lower-case-with-dash", "lower-case-with-dash"},
+                {"lower-case-with_number-123", "lower-case-withNumber-123"},
+                {"lower-case_with_other_char-!@#$", "lower-caseWithOtherChar-!@#$"},
+                {"ALL_UPPER_CASE", "allUpperCase"},
+                {"__the_heart_of_life-_John-Mayer__", "theHeartOfLife-John-mayer"}
+        };
+    }
+
+    @Test(groups = "fast", dataProvider = "testToCamelCase")
+    public void testToCamelCase(final String sample, final String result) {
+        final String camelCase = Strings.toCamelCase(sample, false, '_');
+        // Test compatibility with Guava CaseFormat . Used in LowerToCamelBeanMapper.java
+        // final String camelCase = com.google.common.base.CaseFormat.LOWER_UNDERSCORE.to(com.google.common.base.CaseFormat.LOWER_CAMEL, sample);
+        Assert.assertEquals(camelCase, result);
+    }
+
+    @DataProvider(name = "testToSnakeCase")
+    public Object[][] testToSnakeCaseData() {
+        return new Object[][] {
+                {"thisIsASentence", "this_is_a_sentence"},
+                {"thisIsLowerCase", "this_is_lower_case"},
+                {"lower-case-with-dash", "lower-case-with-dash"},
+                {"lower-case-withNumber-123", "lower-case-with_number-123"},
+                {"lower-caseWithOtherChar-!@#$", "lower-case_with_other_char-!@#$"},
+                {"theHeartOfLife-John-mayer", "the_heart_of_life-_john-mayer"}
+        };
+    }
+
+    @Test(groups = "fast", dataProvider = "testToSnakeCase")
+    public void testToSnakeCase(final String sample, final String result) {
+        final String snakeCase = Strings.toSnakeCase(sample);
+        // Test compatibility with Guava CaseFormat . Used in LowerToCamelBeanMapper.java
+        // final String snakeCase = com.google.common.base.CaseFormat.LOWER_CAMEL.to(com.google.common.base.CaseFormat.LOWER_UNDERSCORE, sample);
+        Assert.assertEquals(snakeCase, result);
+    }
 }
