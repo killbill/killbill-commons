70cde0ff69b03b8051fd8f76cd20a7f88b30c34f
xsalefter <xsalefter@gmail.com>
2026-04-11 01:21:46 +0700

    jooby: move metrics module from javax to jakarta namespace

--- metrics/src/main/java/org/killbill/commons/metrics/servlets/ThreadDumpServlet.java
+++ metrics/src/main/java/org/killbill/commons/metrics/servlets/ThreadDumpServlet.java
@@ -29,9 +29,9 @@ import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class ThreadDumpServlet extends HttpServlet {
 
