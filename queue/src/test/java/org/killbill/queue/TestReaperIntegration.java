5d6f1bed8f1dc631bda5c6f407af2aa691066b25
xsalefter <xsalefter@gmail.com>
2026-05-07 00:04:50 +0700

    fix broken test because 15s wait is not enough
    The error message: Condition returned by method "waitFor" in class org.killbill.queue.TestReaperIntegration$DummyHandler was not fulfilled within 15 seconds.

--- queue/src/test/java/org/killbill/queue/TestReaperIntegration.java
+++ queue/src/test/java/org/killbill/queue/TestReaperIntegration.java
@@ -339,7 +339,7 @@ public class TestReaperIntegration extends TestSetup {
         }
 
         void waitFor(final BusEvent event) {
-            Awaitility.await().atMost(15, TimeUnit.SECONDS).until(new Callable<Boolean>() {
+            Awaitility.await().atMost(30, TimeUnit.SECONDS).until(new Callable<Boolean>() {
                 @Override
                 public Boolean call() throws Exception {
                     return receivedEvents.contains(event.getUserToken());
